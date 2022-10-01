b430f53320813df28b797868ae0dd03edc517b77
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2022-10-01 19:59:36 +0330

    Init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
+);
+
+export default App;
