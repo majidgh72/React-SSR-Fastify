b0a598231a9b2bc3c890ae99cf93a89ede4b569d
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2022-10-01 19:17:03 +0330

    uninstall express

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import About from "./About";
 import Home from "./Home";
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
   </Routes>
 );
 
